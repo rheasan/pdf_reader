728cdb57c5c055af2f608a3452f05ead26da79e7
common <common.home.ac@gmail.com>
2023-01-27 00:24:18 +0530

    switched to CKEditor

--- src/components/Notebook.tsx
+++ src/components/Notebook.tsx
@@ -1,38 +1,31 @@
+import React, { Component } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-
-import React, { useRef } from 'react';
- import { Editor } from '@tinymce/tinymce-react';
 const Notebook = () => {
-    const editorRef = useRef(null);
-   const log = () => {
-     if (editorRef.current) {
-       console.log(editorRef.current!.getContent());
-     }
-   };
-    return (
-        <div className="border-3 border-white text-white grow h-full">
-            
-    <Editor
-        onInit={(evt, editor) => editorRef.current = editor}
-        initialValue="<p>This is the initial content of the editor.</p>"
-        init={{
-        height: 500,
-        menubar: true,
-        plugins: [
-           'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
-           'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
-           'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
-        ],
-        toolbar: 'undo redo | casechange blocks | bold italic backcolor | ' +
-           'alignleft aligncenter alignright alignjustify | ' +
-           'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help',
-        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-        }}
-    />
-    <button onClick={log}>Log editor content</button>
-    
-        </div>
-    )
+        return (
+            <div className="App">
+                <h2>Using CKEditor 5 build in React</h2>
+                <CKEditor
+                    editor={ ClassicEditor }
+                    data="<p>Hello from CKEditor 5!</p>"
+                    onReady={ editor => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log( 'Editor is ready to use!', editor );
+                    } }
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        console.log( { event, editor, data } );
+                    } }
+                    onBlur={ ( event, editor ) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                        console.log( 'Focus.', editor );
+                    } }
+                />
+            </div>
+        );
 }
 
 export default Notebook;
