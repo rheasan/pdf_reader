93473bec17c3e0be57e223c29c17e4eecbff14f9
common <common.home.ac@gmail.com>
2023-01-26 23:54:12 +0530

    added rte

--- src/components/Notebook.tsx
+++ src/components/Notebook.tsx
@@ -1,8 +1,36 @@
+
+
+import React, { useRef } from 'react';
+ import { Editor } from '@tinymce/tinymce-react';
 const Notebook = () => {
-
+    const editorRef = useRef(null);
+   const log = () => {
+     if (editorRef.current) {
+       console.log(editorRef.current!.getContent());
+     }
+   };
     return (
-        <div className="grow border-1 border-black Notebook">
-
+        <div className="border-3 border-white text-white grow h-full">
+            
+    <Editor
+        onInit={(evt, editor) => editorRef.current = editor}
+        initialValue="<p>This is the initial content of the editor.</p>"
+        init={{
+        height: 500,
+        menubar: true,
+        plugins: [
+           'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
+           'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
+           'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
+        ],
+        toolbar: 'undo redo | casechange blocks | bold italic backcolor | ' +
+           'alignleft aligncenter alignright alignjustify | ' +
+           'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help',
+        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+        }}
+    />
+    <button onClick={log}>Log editor content</button>
+    
         </div>
     )
 }
