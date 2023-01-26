a73a9bc7512f3ccfb6ac8d12251b45c72561afe8
Shreyas Chavan <shreyaschavan228a@gmail.com>
2023-01-27 02:34:22 +0530

    fix ckeditor styles

--- src/components/Notebook.tsx
+++ src/components/Notebook.tsx
@@ -1,31 +1,23 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 const Notebook = () => {
-        return (
-            <div className="h-full">
-                <h2>Using CKEditor 5 build in React</h2>
-                <CKEditor
-                    editor={ ClassicEditor }
-                    data="<p>Hello from CKEditor 5!</p>"
-                    onReady={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( event, editor ) => {
-                        const data = editor.getData();
-                        console.log( { event, editor, data } );
-                    } }
-                    onBlur={ ( event, editor ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        console.log( 'Focus.', editor );
-                    } }
-                />
-            </div>
-        );
+
+    return (
+        <div className="h-full w-full">
+            <h2 className="block h-10">Using CKEditor 5 build in React</h2>
+            <CKEditor
+                editor={ClassicEditor}
+                data="<p>Hello from CKEditor 5!</p>"
+                onChange={(event, editor) => {
+                    const data = editor.getData();
+                    console.log({ event, editor, data });
+                }}
+                className="h-full"
+            />
+        </div>
+    );
 }
 
 export default Notebook;
