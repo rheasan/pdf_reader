e8a283ed5abe37d6bcc3127c42ea18938e21c522
Shreyas Chavan <shreyaschavan228a@gmail.com>
2023-01-26 19:55:18 +0530

    add notebook component

---
+++ src/components/Notebook.tsx
@@ -0,0 +1,10 @@
+const Notebook = () => {
+
+    return (
+        <div className="grow border-1 border-black Notebook">
+
+        </div>
+    )
+}
+
+export default Notebook;
--- src/components/Pdf_Render.tsx
+++ src/components/Pdf_Render.tsx
@@ -2,6 +2,7 @@ import { SetStateAction, useEffect, useRef, useState } from "react";
 import { Document, Page } from "react-pdf/dist/esm/entry.vite";
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
+import Notebook from "./Notebook";
 
 const Pdf_Render = (props: { file: File; setValid: React.Dispatch<SetStateAction<boolean>> }) => {
     const { file, setValid } = props;
@@ -13,12 +14,15 @@ const Pdf_Render = (props: { file: File; setValid: React.Dispatch<SetStateAction
         setNumPages(pdf.numPages);
     }
     return (
-        <div>
-            <Document file={file} onLoadSuccess={onDocumentLoadSuccess} renderMode="svg">
-            {Array.from(new Array(numPages), (el, index) => (
-              <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-            ))}
-          </Document>
+        <div className="flex flex-row">
+            <div className="w-fit">
+                <Document file={file} onLoadSuccess={onDocumentLoadSuccess} renderMode="svg">
+                    {Array.from(new Array(numPages), (el, index) => (
+                        <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+                    ))}
+                </Document>
+            </div>
+            <Notebook />
         </div>
     )
 }
