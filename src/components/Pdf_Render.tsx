54f12f78ad973964b08d61feed3b1fd8b08d617b
Shreyas Chavan <shreyaschavan228a@gmail.com>
2023-01-26 23:20:34 +0530

    remove unused vars and add close function

--- src/components/Pdf_Render.tsx
+++ src/components/Pdf_Render.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction, useEffect, useRef, useState } from "react";
+import { SetStateAction, useRef, useState } from "react";
 import { Document, Page } from "react-pdf/dist/esm/entry.vite";
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
@@ -7,18 +7,23 @@ import Notebook from "./Notebook";
 const Pdf_Render = (props: { file: File; setValid: React.Dispatch<SetStateAction<boolean>> }) => {
     const { file, setValid } = props;
     const [numPages, setNumPages] = useState(0);
-    const [pageNumber, setPageNumber] = useState(1);
-    const canvasRef = useRef(null);
 
-    const onDocumentLoadSuccess = (pdf) => {
+    const onDocumentLoadSuccess = (pdf: { numPages: SetStateAction<number>; }) => {
         setNumPages(pdf.numPages);
     }
     return (
         <div className="flex flex-row">
             <div className="w-fit">
+                <div className="h-10 w-full flex flex-row justify-between text-white m-0 p-1">
+                    <h1 className="block">{file.name}</h1>
+                    <button className="w-fit m-0 basis-1 block" onClick={() => setValid(false)}>Close</button>
+                </div>
                 <Document file={file} onLoadSuccess={onDocumentLoadSuccess} renderMode="svg">
                     {Array.from(new Array(numPages), (el, index) => (
-                        <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+                        <div>
+                            <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+                            <div className="h-2 bg-black m-0"></div>
+                        </div>
                     ))}
                 </Document>
             </div>
