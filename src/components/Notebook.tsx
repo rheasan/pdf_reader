1acd925a693be64feedc2b81d6e6229645b4c9b8
Shreyas Chavan <shreyaschavan228a@gmail.com>
2023-01-27 14:38:06 +0530

    export notebook as html added

--- src/components/Notebook.tsx
+++ src/components/Notebook.tsx
@@ -4,15 +4,28 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 const Notebook = () => {
 
+    const [data, setData] = useState("");
+
+    const handleNotebookDownload = () => {
+        const anchor = document.createElement("a");
+        anchor.style.display = "hidden";
+        anchor.download = "notebook.html";
+        anchor.href = "data:text/plain;charset=utf-8," + encodeURIComponent(data);
+        document.body.appendChild(anchor);
+        anchor.click();
+        document.body.removeChild(anchor);
+    }
+
     return (
         <div className="h-full w-full">
-            <h2 className="block h-10">Using CKEditor 5 build in React</h2>
+            <div className="h-10 w-full flex flex-row justify-between text-white m-0 p-1">
+                <h1 className="block" onClick={handleNotebookDownload}>Export Notebook</h1>
+            </div>
             <CKEditor
                 editor={ClassicEditor}
                 data="<p>Hello from CKEditor 5!</p>"
                 onChange={(event, editor) => {
-                    const data = editor.getData();
-                    console.log({ event, editor, data });
+                    setData(editor.getData());
                 }}
                 className="h-full"
             />
